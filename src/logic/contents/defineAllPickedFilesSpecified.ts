bed435cc8b1c43ad5bd3de70b07299486807773c
Ulad Kasach <uladkasach@gmail.com>
2025-11-28 19:08:14 -0600

    fix(practs): bump to latest best

--- src/logic/contents/defineAllPickedFilesSpecified.ts
+++ src/logic/contents/defineAllPickedFilesSpecified.ts
@@ -10,6 +10,7 @@ export const defineAllPickedFilesSpecified = async ({
   // run glob against each glob given and pull back the files specified
   const filePaths = await fastGlob(pickFileGlobs, {
     cwd: projectRootDirectory,
+    dot: true, // include files and directories starting with a dot (e.g., .next/)
   });
   return filePaths.sort();
 };
